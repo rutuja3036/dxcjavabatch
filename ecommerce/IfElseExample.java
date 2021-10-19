abb6c95aa52367e907f793b7b3c7c74f2c641629
rutuja3036 <92770915+rutuja3036@users.noreply.github.com>
2021-10-19 15:00:08 +0530

    Add files via upload
    Control Statement

---
+++ ecommerce/IfElseExample.java
@@ -0,0 +1,15 @@
+package ecommerce;
+
+public class IfElseExample {
+	public static void main(String[] args) {
+		int number = 13;
+		if(number%2==0) {
+			System.out.println("Even Number");
+			
+		}
+		else {
+			System.out.println("odd number");
+		}
+	}
+
+}
---
+++ ecommerce/IfExample.java
@@ -0,0 +1,11 @@
+package ecommerce;
+
+public class IfExample {
+	public static void main(String[] args) {
+		int age=20;
+		if (age>18);{
+			System.out.println("Age is greter than 18");
+		}
+	}
+
+}
